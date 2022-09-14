cffc1dda6f5f4add400ad2d4ac6494b52c5caf05
ooAkira <lee.quockhanh1107@gmail.com>
2022-09-14 17:01:06 +0700

    style input register

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import "./style.scss"
+import "./style.scss";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
-function App() {
-  return (
-    <Register />
-    
-  );
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,37 +1,51 @@
 import { useState } from 'react';
-import Add from "../img/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "@firebase/auth";
-import { auth, storage } from "../firebase";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { async } from '@firebase/util';
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "@firebase/storage";
+import { doc, setDoc } from "@firebase/firestore"
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
   const handleSubmit = async (e:any) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    const file = e.target[3].value;
+    const file = e.target[3].files[0];
 
     try{
+      // Create user
       const res = await createUserWithEmailAndPassword(auth, email, password)
       
-      const storage = getStorage();
+      // Create a unique image name
       const storageRef = ref(storage, displayName);
 
       const uploadTask = uploadBytesResumable(storageRef, file);
 
+      // Register three observer
       uploadTask.on('state_changed', 
-        (error) => {
-          setErr(true);
-        }, 
+        (error) => setErr(true), 
         () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-            await updateProfile(res.user,{
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            //Update profile
+            await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
-            })
+            });
+
+            // Create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate('/');
           });
         }
       );
@@ -51,7 +65,7 @@ const Register = () => {
             <input type="password" placeholder="password"/>
             <input style={{display:"none"}} type="file" id="file"/>
             <label htmlFor="file">
-              <img src={Add} alt="" />
+              <img src={require('../img/addAvatar.png')} alt="addImage" />
               <span>Add an avatar</span>
             </label>
             <button>Sign up</button>
